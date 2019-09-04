ba3865a1e7da3d26e8706e7b83cb58dcef3fe8e7
kallyous <kallyous@gmail.com>
2019-09-04 16:13:14 -0300

    Ajusta tamanho da variavel final_length

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ int main(int argc, byte * argv[])
     // Interpreta e valida argumentos recebidos
     if (!parse_arguments(argc, argv)) return 255;
 
-    unsigned long final_length;
+    unsigned long long final_length;
 
     // Boas vindas
     printf("-------------------------------------------------------------------------\n");
@@ -34,15 +34,17 @@ int main(int argc, byte * argv[])
         dfprint("Rotina de compactação escolhida.\n");
         printf("Compactadno %s ...\n", FILE_NAME_STR);
         final_length = pack(); }
+
     else if (OPERATION == 'U') {
         dfprint("Rotina de descompactação escolhida.\n");
-        printf("Descompactadno %s ...\n", FILE_NAME_STR);
+        printf("Descompactando %s ...\n", FILE_NAME_STR);
         final_length = unpack(); }
+
     else {
         dfprint("Rotina não definida, encerrando...\n");
         return 255; }
 
-    printf("\nEscrito %lu bytes.\n", final_length);
+    printf("\nEscrito %llu bytes.\n", final_length);
     printf("-------------------------------------------------------------------------\n");
 
     return 0;
